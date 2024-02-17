93836266d3f8a58959c1badbce850dfd07cd04d2
MarlonChi <marlonchiodelli@hotmail.com>
2024-02-16 22:21:49 -0300

    feat: componentization and use context

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+
+import { CyclesContext } from "../..";
+
+import * as S from "./styles";
+
+export const NewCycleForm = () => {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
+  return (
+    <S.FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <S.TaskInput
+        type="text"
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <S.MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={1}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </S.FormContainer>
+  );
+};
